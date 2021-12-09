0d86fe083ffe5dd8b865b59d03339cd82f1b3660
Muna Dahir <muna.dahir@hotmail.com>
2021-12-08 23:55:07 -0500

    change list variable name

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,8 +4,8 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
   
   const {days, onChange} = props
-  const parsedDays = days.map((day) => <DayListItem key={day.id} {...day} selected={day.name === props.value} setDay={onChange}/> )
+  const dayList = days.map((day) => <DayListItem key={day.id} {...day} selected={day.name === props.value} setDay={onChange}/> )
   return <ul>
-   {parsedDays}
+   {dayList}
   </ul>
 }
