25dd52083c6b6617dfd72a26b6e2235135330370
Muna Dahir <muna.dahir@hotmail.com>
2021-12-09 16:36:55 -0500

    intall and import axios into Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState }  from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from './DayList';
 import "components/Application.scss";
 import Appointment from "components/Appointment"
