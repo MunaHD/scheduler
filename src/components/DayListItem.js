d3ee8b8596ecd0d74d2789dd3ccd4f8bff4ebf91
Muna Dahir <muna.dahir@hotmail.com>
2021-12-08 23:48:19 -0500

    add classnames and event handler to component with props

---
+++ src/components/DayListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+
+  let dayClass = classNames("day-list__item ", 
+  {"day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0})
+  
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return "no spots remaining"
+    } else if (spots === 1) {
+      return "1 spot remaining"
+    } else {
+      return `${spots} spots remaining`
+    }
+  }
+  return (
+    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+      <h2 className="text--regular">{props.name}</h2> 
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  );
+}
