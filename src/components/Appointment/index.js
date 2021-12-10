a0d243fdb2ea10593b49865042b59a9b74cb8613
Muna Dahir <muna.dahir@hotmail.com>
2021-12-10 17:48:58 -0500

    render SHOW / EMPTY contitionally with useVisualMode in index.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,9 +3,20 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from 'hooks/useVisualMode';
+
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 export default function Appointment(props) {
   const { time, interview } = props
+
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
+
+  
   return (
     <article className="appointment">
       <Header time={time} />
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+  //console.log("history", history)
+  function transition(newMode, replace  = false) {
+    if (replace === true) {
+      history.pop()
+    }
+    setHistory([...history, newMode]);
+    setMode(newMode)
+  }
+  function back() {
+    if (history.length > 1) {
+      history.pop()
+      setMode(history[history.length - 1])
+    }
+  }
+
+  return {mode, transition, back}
+
+}
+
+
