0c77b2786f7d5da92e567f5759c3d7cc74156628
Muna Dahir <muna.dahir@hotmail.com>
2021-12-10 19:21:17 -0500

    add CREATE mode to render form with onCancel button

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,23 +4,26 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import useVisualMode from 'hooks/useVisualMode';
-
+import Form from 'components/Appointment/Form';
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
+const CREATE= "CREATE";
 
 export default function Appointment(props) {
+  console.log(props);
   const { time, interview } = props
 
   const { mode, transition, back } = useVisualMode(
     interview ? SHOW : EMPTY
   );
 
-  
   return (
     <article className="appointment">
       <Header time={time} />
-      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+      {mode === EMPTY &&  <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer}/> }
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} /> }
     </article>
     
   )
