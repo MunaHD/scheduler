b538b6f4067037e9ce876c0c26c5dc192e656550
Muna Dahir <muna.dahir@hotmail.com>
2021-12-12 10:15:32 -0500

    add the (prev)to setHistory in transition.  Make a copy of the orignal history to use in back()

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,20 +3,26 @@ import { useState } from 'react';
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
-  
-  //TODO: add prev to setHostory()
-
+ 
   function transition(newMode, replace  = false) {
     if (replace === true) {
-      history.pop()
+      //replacing the pop. So it berings back everything except the last element
+      setHistory(prev => [...prev].slice(0, -1));
     }
-    setHistory([...history, newMode]);
+    // set the history to everything in its prvious state and add newMode
+    setHistory(prev => [...prev, newMode]);
     setMode(newMode)
   }
   function back() {
+    //only when there is a mode aside form the original
     if (history.length > 1) {
-      history.pop()
-      setMode(history[history.length - 1])
+      //make a copy of history becuase the original needs to be
+      // intact to next use of transition function
+      const historyCopy = history.slice(0, -1);
+      setHistory(historyCopy);
+      
+      //the current mode is the last element of the copied history array (the second last of the original)
+      setMode(historyCopy[historyCopy.length - 1])
     }
   }
 
