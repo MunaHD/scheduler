d14f17950802313539101381b6e96439bb3df667
Muna Dahir <muna.dahir@hotmail.com>
2021-12-13 18:12:33 -0500

    add bookinterview function with axios put request

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,13 +7,32 @@ import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "help
 
 export default function Application(props) {
   const setDay = day => setState({ ...state, day });
-
   const [state, setState] = useState({
     day: "Monday",
     days: [],
     appointments: {},
     interviewers: {}
-  })
+  });
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    setState({...state, appointments })
+    
+    return axios.put(`/api/appointments/${id}`, {interview})
+      .then(() => {
+      setState({...state, appointments})
+      })
+    // console.log(id, interview);
+  }
+  
   
   
   useEffect(() => {
@@ -43,6 +62,7 @@ export default function Application(props) {
         {...app}
         interview={interview}
         interviewers={interviewers}
+        bookInterview={bookInterview}
       />)
   } )
 
