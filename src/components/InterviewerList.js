88a48161354ea646f2de2bea45e59b4be0e65fe3
Muna Dahir <muna.dahir@hotmail.com>
2021-12-14 15:42:19 -0500

    add props.type to interviewList

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem"
-
 
 const InterviewerList = (props) => {
   
@@ -16,5 +16,8 @@ const InterviewerList = (props) => {
 </section>
   )
 }
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
 
-export default InterviewerList
+export default InterviewerList;
